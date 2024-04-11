4a8fb232c9d707225153dc819bb2867ce885d7ef
Nestor Garcia <nestorisok@gmail.com>
2024-04-11 13:45:13 -0700

    Complete

--- dynamicProgramming/dynamicProgramming.cpp
+++ dynamicProgramming/dynamicProgramming.cpp
@@ -3,7 +3,6 @@
 
 using namespace std;
 
-const int n = 1000;
 int matrix_chain_order(int p[], int n);
 void print_opt_parens(int **s, int i, int j, char& name);
 
@@ -11,12 +10,22 @@ void print_opt_parens(int **s, int i, int j, char& name);
 
 int main() {
 	
-	cout << "Test" << endl;
-	int testArr[] = { 4, 10, 3, 12, 20, 7 };
-	const int arrSize = sizeof(testArr) / sizeof(int);
-	// cout << "Size: " << arrSize << endl;
+	//cout << "Test" << endl;
 
+	cout << "Sample Input 1: ";
+	int testArr[] = { 10, 20, 30, 40, 30 };
+	const int arrSize = sizeof(testArr) / sizeof(int);
 	matrix_chain_order(testArr, arrSize);
+
+
+	cout << "\n\nSample Input 2: ";
+	int testArr2[] = { 4, 10, 3, 12, 20, 7 };
+	const int arrSize2 = sizeof(testArr2) / sizeof(int);
+	// cout << "Size: " << arrSize << endl;
+	matrix_chain_order(testArr2, arrSize2);
+	
+
+
 	
 
 
@@ -40,7 +49,7 @@ int matrix_chain_order(int p[], int n) {
 
 	//for (int i = 0; i < n; i++) {
 
-	//	for (int j = 0; j < n; j++) {
+	//	for (int j = 0; j < n; j++) { // testing to see if initial creation of new arr were filled with 0s
 	//		m[i][j] = 0; // collumns
 	//		s[i][j] = 0;
 
@@ -53,7 +62,7 @@ int matrix_chain_order(int p[], int n) {
 		for (int i = 1; i < n - l + 1; i++) {
 			
 			int j = i + l - 1;
-			m[i][j] = INT_MAX;
+			m[i][j] = INT_MAX; // 'infinity' , i used the maximums numbers allowed for ints on compiler
 			
 			for (int k = i; k <= j - 1; k++) {
 				int q = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j];
@@ -71,9 +80,9 @@ int matrix_chain_order(int p[], int n) {
 	print_opt_parens(s, 1, n-1, name); // we need n - 1 because we aren't using the final collumn/first row
 	cout << "\nOptimal Cost is: " << m[1][n - 1]; // same issue here
 
-
-	delete[] m[n]; //reallocating memory
-	delete[] s[n - 1];
+	//reallocating memory
+	//delete[] m[n];
+	//delete[] s[n - 1];
 	return 0;
 }
 
@@ -91,7 +100,7 @@ void print_opt_parens(int **s, int i, int j, char &name) {
 
 
 
-
+// using [i, j] instead of [i][j], personal error from reading psuedocode
 
 //int matrix_chain_order(int p[], const int n) {
 //	int** m = new int* [n]; // rows for s
